a88ec7bde630a5d9412e649bfbc202b19641b7dc
cezarfbc <cezarfbc@gmail.com>
2024-08-10 16:17:44 -0300

    falta servicos e contato

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    colors: {
+      black: '#262526',
+      white: '#FFFFFF',
+      dark_white: '#D9D9D9',
+      green: '#107345',
+      light_green: '#0E733A',
+      dark_green: '#1D593D',
+    },
+  },
+  plugins: [],
+};
