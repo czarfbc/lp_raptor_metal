0742b307229605446c8a3503f9b2cf387255d6ff
cezarfbc <cezarfbc@gmail.com>
2024-08-11 17:20:20 -0300

    header arrumado

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+  let headerScroll = document.getElementById('header');
+  headerScroll.classList.toggle('scroll', window.scrollY > 30);
+
+  let headerMenuScroll = document.getElementById('header_menu');
+  headerMenuScroll.classList.toggle('scroll_menu', window.scrollY > 30);
+
+  let headerMenuScrollImage = document.getElementById('image_header_menu');
+  headerMenuScrollImage.classList.toggle('scroll_image', window.scrollY > 30);
+});
