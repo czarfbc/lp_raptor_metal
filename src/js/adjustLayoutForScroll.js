79c8caf594d2dae924ba308f21c4e9e997130e7a
cezarfbc <cezarfbc@gmail.com>
2024-08-14 19:22:42 -0300

    concluido 2

--- src/js/adjustLayoutForScroll.js
+++ src/js/adjustLayoutForScroll.js
@@ -1,15 +1,13 @@
 function adjustLayoutForScroll() {
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 30) {
-      let headerScroll = document.getElementById('header');
-      headerScroll.classList.toggle('scroll');
+    let headerScroll = document.getElementById('header');
+    headerScroll.classList.toggle('scroll', window.scrollY > 30);
 
-      let headerMenuScroll = document.getElementById('header_menu');
-      headerMenuScroll.classList.toggle('scroll_menu');
+    let headerMenuScroll = document.getElementById('header_menu');
+    headerMenuScroll.classList.toggle('scroll_menu', window.scrollY > 30);
 
-      let headerMenuScrollImage = document.getElementById('image_header_menu');
-      headerMenuScrollImage.classList.toggle('scroll_image');
-    }
+    let headerMenuScrollImage = document.getElementById('image_header_menu');
+    headerMenuScrollImage.classList.toggle('scroll_image', window.scrollY > 30);
   });
 }
 
